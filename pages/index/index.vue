acfc402be77284e342f7df65ff7e9ff21fd8be1c
chenqinhao <ch907941189@icloud.com>
2020-08-31 14:21:14 +0800

    first commit

---
+++ pages/index/index.vue
@@ -0,0 +1,26 @@
+<template>
+	<view class="container">
+		<!--  使用 web-view 映射自己的自定义 html -->
+		<web-view src="../../static/index.html"></web-view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.container {
+		padding: 20px;
+		font-size: 14px;
+		line-height: 24px;
+	}
+</style>
